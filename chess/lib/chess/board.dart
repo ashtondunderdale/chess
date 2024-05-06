6c9545aec44bcf9c43f982583a875b5a76035faf
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-05-06 19:34:54 +0100

    board format

--- chess/lib/chess/board.dart
+++ chess/lib/chess/board.dart
@@ -10,18 +10,28 @@ class Board extends StatefulWidget {
 class _BoardState extends State<Board> {
   @override
   Widget build(BuildContext context) => Scaffold(
-    body: GridView.builder(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 8,
+    backgroundColor: Colors.white,
+    body: Center(
+      child: Container(
+        width: 700,
+        height: 700,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black, width: 2)
+        ),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 8,
+          ),
+          itemBuilder: (BuildContext context, int index) {
+            int row = index ~/ 8;
+            int col = index % 8;
+            Color color = (row + col) % 2 == 0 ? Colors.white : Colors.black;
+        
+            return Container(color: color);
+          },
+          itemCount: 64,
+        ),
       ),
-      itemBuilder: (BuildContext context, int index) {
-        int row = index ~/ 8;
-        int col = index % 8;
-        Color color = (row + col) % 2 == 0 ? Colors.white : Colors.black;
-
-        return Container(color: color);
-      },
-      itemCount: 64,
     ),
   );
 }
