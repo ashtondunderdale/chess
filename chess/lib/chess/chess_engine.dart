686f21f0e50da585dec998b0dec5e406f75b3865
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-05-22 18:26:56 +0100

    fixed pawn movement

--- chess/lib/chess/chess_engine.dart
+++ chess/lib/chess/chess_engine.dart
@@ -30,14 +30,19 @@ class ChessEngine {
     switch (piece.type) {
       case PieceType.pawn:
         return isValidPawnMove(piece, destinationRow, destinationColumn, boardState);
+
       case PieceType.rook:
         return isValidRookMove(piece, destinationRow, destinationColumn, boardState);
+
       case PieceType.knight:
         return isValidKnightMove(piece, destinationRow, destinationColumn, boardState);
+
       case PieceType.bishop:
         return isValidBishopMove(piece, destinationRow, destinationColumn, boardState);
+
       case PieceType.queen:
         return isValidQueenMove(piece, destinationRow, destinationColumn, boardState);
+
       case PieceType.king:
         return isValidKingMove(piece, destinationRow, destinationColumn, boardState);
     }
@@ -48,17 +53,42 @@ class ChessEngine {
     final currentColumn = piece.column;
     final color = piece.color;
 
-    if (color == Colors.white) {
-      if (destinationRow == currentRow - 1 && destinationColumn == currentColumn) {
-        return boardState[destinationRow][destinationColumn] == null;
-      }
-    } else {
-      if (destinationRow == currentRow + 1 && destinationColumn == currentColumn) {
-        return boardState[destinationRow][destinationColumn] == null;
+    final isCapture = boardState[destinationRow][destinationColumn] == null;
+
+    List<List<int>> validMoves = [];
+
+    for (var row = 0; row < 8; row++) {
+      for (var column = 0; column < 8; column++) {
+        //var square = boardState[row][column];
+
+        if (column == currentColumn ) {
+          validMoves.add([row, column]);
+        }
+
+        // if () {
+
+        // }
       }
     }
 
-    return false;
+    bool containsDestination = validMoves.any((move) =>
+      move[0] == destinationRow && move[1] == destinationColumn
+    );
+
+    return containsDestination;
+
+
+    // if (color == Colors.white) {
+    //   if (destinationRow == currentRow - 1 && destinationColumn == currentColumn) {
+    //     return boardState[destinationRow][destinationColumn] == null;
+    //   }
+    // } else {
+    //   if (destinationRow == currentRow + 1 && destinationColumn == currentColumn) {
+    //     return boardState[destinationRow][destinationColumn] == null;
+    //   }
+    // }
+
+    // return false;
   }
   
   static bool isValidRookMove(ChessPiece piece, int destinationRow, int destinationColumn, List<List<ChessPiece?>> boardState) {
@@ -79,5 +109,13 @@ class ChessEngine {
   
   static bool isValidKingMove(ChessPiece piece, int destinationRow, int destinationColumn, List<List<ChessPiece?>> boardState) {
     return true;
+  }
+
+  bool isSameColumn(int currentColumn, int destinationColumn) {
+  return currentColumn == destinationColumn;
+}
+
+  bool isDestinationEmpty(int destinationRow, int destinationColumn, List<List<ChessPiece?>> boardState) {
+    return boardState[destinationRow][destinationColumn] == null;
   }
 }
